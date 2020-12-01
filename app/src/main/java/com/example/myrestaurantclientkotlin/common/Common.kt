3054fbd3c59297da5068aef19ed41bd152b58d30
SiThuc <hthucvnsoft@gmail.com>
2020-12-01 13:33:58 +0100

    Authentication by Phone

---
+++ app/src/main/java/com/example/myrestaurantclientkotlin/common/Common.kt
@@ -0,0 +1,8 @@
+package com.example.myrestaurantclientkotlin.common
+
+import com.example.myrestaurantclientkotlin.model.UserModel
+
+object Common {
+    val USER_REF: String = "Clients"
+    var currentUser: UserModel? = null
+}
---
+++ app/src/main/java/com/example/myrestaurantclientkotlin/model/UserModel.kt
@@ -0,0 +1,16 @@
+package com.example.myrestaurantclientkotlin.model
+
+class UserModel {
+    var uid: String? = null
+    var name: String? = null
+    var address: String? = null
+    var phone: String? = null
+
+    constructor() {}
+    constructor(uid: String?, name: String?, address: String?, phone: String?) {
+        this.uid = uid
+        this.name = name
+        this.address = address
+        this.phone = phone
+    }
+}
